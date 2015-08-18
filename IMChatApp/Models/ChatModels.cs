27349d8cd0c528c540bb6a0ae237f99315f91fdf
sharadit47 <sharadit2011@gmail.com>
2015-08-18 23:51:21 +0530

    Multiple windows

--- IMChatApp/Models/ChatModels.cs
+++ IMChatApp/Models/ChatModels.cs
@@ -7,11 +7,71 @@ using System.Web;
 
 namespace IMChatApp.Models
 {
-    public class Login {
+    public class ExternalLoginConfirmationViewModel
+    {
         [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class LoginViewModel
+    {
+       // [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]        
         [Display(Name = "Name")]
         public string UserNick { get; set; }
+
+        [Required]
+        [Display(Name = "User Type")]
+        public bool UserType { get; set; }
+
+       // [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+       // [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
     }
   public enum Status
     {
@@ -46,6 +106,7 @@ namespace IMChatApp.Models
         public string status { get; set; }
         public string memberType { get; set; }
         public string avator { get; set; }
+            public string ContextName { get; set; }
 
         //public user Login(string UserName)
         //{ 
